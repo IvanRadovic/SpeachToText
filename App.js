8cfb8540bb14fba95937d41b73fb24c756a3d7bf
Ivek <iradovic01@yahoo.com>
2025-03-03 10:19:52 +0100

    Starting project - everything working

--- App.js
+++ App.js
@@ -1,13 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react'
+import { StyleSheet, Text, View, Button, ScrollView } from 'react-native'
+import {
+  ExpoSpeechRecognitionModule,
+  useSpeechRecognitionEvent,
+} from 'expo-speech-recognition'
 
 export default function App() {
+  const [recognizing, setRecognizing] = useState(false)
+  const [transcript, setTranscript] = useState('')
+
+  useSpeechRecognitionEvent('start', () => setRecognizing(true))
+  useSpeechRecognitionEvent('end', () => setRecognizing(false))
+  useSpeechRecognitionEvent('result', (event) => {
+    setTranscript(event.results[0]?.transcript)
+  })
+  useSpeechRecognitionEvent('error', (event) => {
+    console.log('error code:', event.error, 'error message:', event.message)
+  })
+
+  const handleStart = async () => {
+    const result = await ExpoSpeechRecognitionModule.requestPermissionsAsync()
+    if (!result.granted) {
+      console.warn('Permissions not granted', result)
+      return
+    }
+    // Start speech recognition
+    ExpoSpeechRecognitionModule.start({
+      lang: 'hr-HR',
+      interimResults: true,
+      maxAlternatives: 1,
+      continuous: false,
+      requiresOnDeviceRecognition: false,
+      addsPunctuation: false,
+      contextualStrings: ['Carlsen', 'Nepomniachtchi', 'Praggnanandhaa'],
+    })
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {!recognizing ? (
+        <Button title="Start" onPress={handleStart} />
+      ) : (
+        <Button
+          title="Stop"
+          onPress={() => ExpoSpeechRecognitionModule.stop()}
+        />
+      )}
+
+      <ScrollView style={{ flex: 1 }}>
+        <Text>{transcript}</Text>
+      </ScrollView>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +61,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
